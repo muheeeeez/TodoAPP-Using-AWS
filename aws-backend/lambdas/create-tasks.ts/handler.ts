a7e16099e3e32bc624c56ee2f8190170bbb5b097
Abdul-muiz Olaleye <abdulmuiz.olatunbosun@gmail.com>
2025-12-13 19:31:42 -0500

    Refactor create-tasks Lambda handler to use randomUUID for task ID generation and improve response structure

--- aws-backend/lambdas/create-tasks.ts/handler.ts
+++ aws-backend/lambdas/create-tasks.ts/handler.ts
@@ -1,27 +1,28 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { randomUUID } from 'crypto';
 
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
-import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
-
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   console.log("EVENT:", event);
   const userId = "mock-user-id";
   const body = event.body ? JSON.parse(event.body) : {};
   const {title, description} = body;
-  const taskId = crypto.randomUUID()
-  const createdAt = new Date().toISOString()
-  const task ={
+  const taskId = randomUUID();
+  const createdAt = new Date().toISOString();
+  const task = {
     userId, taskId, title, description, status: 'pending', createdAt
-  }
+  };
   await ddbDocClient.send(new PutCommand({
     TableName: process.env.TASKS_TABLE_NAME!,
-    Item:task, 
-  }))
-  return{
+    Item: task, 
+  }));
+  return {
     statusCode: 201,
-    body:JSON.stringify(task)
-  }
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(task)
+  };
 };
